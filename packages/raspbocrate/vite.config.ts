6d03e69befb53dd8b368d6eba3463d6c045ba888
John Ferlito <johnf@inodes.org>
2026-02-04 10:38:14 +1100

    feat: Dockerfile for raspbocrate

--- packages/raspbocrate/vite.config.ts
+++ packages/raspbocrate/vite.config.ts
@@ -4,16 +4,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig, type PluginOption } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+// import viteTsConfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 const config = defineConfig({
   plugins: [
-    tanstackStart({
-      spa: {
-        enabled: true,
-      },
-    }),
+    tanstackStart(),
     nitro(),
     viteReact(),
 
