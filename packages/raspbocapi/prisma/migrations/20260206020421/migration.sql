0672b730a0f617ec1018d181fb62d3980e2c71fe
John Ferlito <johnf@inodes.org>
2026-02-06 13:21:58 +1100

    fix(raspbocapi): drop unused rocrate column from Entity table

---
+++ packages/raspbocapi/prisma/migrations/20260206020421/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rocrate` on the `Entity` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Entity` DROP COLUMN `rocrate`;
