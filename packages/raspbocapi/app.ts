52d867b5591e0125fe892e81bc6e2d91b054ffc9
John Ferlito <johnf@inodes.org>
2026-02-07 13:44:08 +1100

    fix: raspocapi listneing on 0/0

--- packages/raspbocapi/app.ts
+++ packages/raspbocapi/app.ts
@@ -204,7 +204,7 @@ await fastify.register(arocapi, {
 console.log('🪚 ♓');
 
 try {
-  await fastify.listen({ port: 4000 });
+  await fastify.listen({ port: 4000, host: '0.0.0.0' });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
